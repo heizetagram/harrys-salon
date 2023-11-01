a3676bed2555169095f28c28e234ddd545acdcd9
David Lu <davidlu1999@gmail.com>
2023-11-01 13:17:09 +0100

    Added promptDate methods, removed viewAvailableDates()

--- appointment/ModifyAppointment.java
+++ appointment/ModifyAppointment.java
@@ -1,6 +1,9 @@
 package appointment;
 
+import date.PromptDate;
+import date.SortDate;
 import harryssalon.Main;
+import ui.ConsoleColors;
 import ui.SystemMessages;
 import ui.UI;
 
@@ -13,6 +16,26 @@ public class ModifyAppointment {
     private List<Appointment> updatedAppointments;
     private SystemMessages systemMessages;
     private SortDate sortDate;
+    private PromptDate promptDate;
+    private String customerName;
+    private String stringUserYear;
+    private String stringUserMonth;
+    private String stringUserDay;
+    private String stringUserHour;
+    private String stringUserMinute;
+    private String currentYear;
+    private String currentMonth;
+    private String currentDay;
+    private String currentHour;
+    private String currentMinute;
+    private String currentProduct;
+    private String newName;
+    private String newYear;
+    private String newMonth;
+    private String newDay;
+    private String newHour;
+    private String newMinute;
+    private String newProduct;
 
     // Constructor
     public ModifyAppointment(Main main) {
@@ -20,53 +43,28 @@ public class ModifyAppointment {
         fileHandlingAppointment = new FileHandlingAppointment(main);
         systemMessages = new SystemMessages(main);
         sortDate = new SortDate(main);
-    }
-
-    // View available dates
-    public void viewAvailableDates() {
-        UI.println("Opening hours: 10-18");
-        UI.println("Enter year (yyyy)");
-        String userYear = Integer.toString(UI.promptInt());
-
-        UI.println("Enter month (m)");
-        String userMonth = Integer.toString(UI.promptInt());
-
-        UI.println("Enter day (d)");
-        String userDay = Integer.toString(UI.promptInt());
-
-        for (Appointment availableDate : main.getAvailableDates())
-            for (Appointment appointment : main.getAppointments()) {
-                if (availableDate.getCustomerName().equals(appointment.getCustomerName()) && availableDate.getYear().equals(appointment.getYear()) && availableDate.getMonth().equals(appointment.getMonth()) && availableDate.getDay().equals(appointment.getDay())) {
-                    System.out.println("test");
-                }
-            }
+        promptDate = new PromptDate();
     }
 
     // Booking of appointments logic
     public void bookAppointment() {
         UI.promptString(); // Scanner bug
-        UI.println("Enter costumer name");
-        String costumerName = UI.promptString();
-        UI.println("Enter appointment year (yyyy)");
-        String year = Integer.toString(UI.promptInt());
-        UI.println("Enter appointment month (m)");
-        String month = Integer.toString(UI.promptInt());
-        UI.println("Enter appointment day (d)");
-        String day = Integer.toString(UI.promptInt());
-        UI.println("Enter appointment hour (h)");
-        String hour = Integer.toString(UI.promptInt());
-        UI.println("Enter appointment minute (m)");
-        String minute = Integer.toString(UI.promptInt());
+        customerName = promptDate.promptCustomerName();
+        stringUserYear = promptDate.promptYear();
+        stringUserMonth = promptDate.promptMonth();
+        stringUserDay = promptDate.promptDay();
+        stringUserHour = promptDate.promptHour();
+        stringUserMinute = promptDate.promptMinute();
+
         double totalPrice = 200;
+
         UI.println("Enter added buys (Shampoo, Balsam, Hairnet");
         UI.promptString(); // Scanner bug
         String addedProduct = UI.promptString();
-
-
         if (addedProduct.isEmpty()) {
             addedProduct = "N/A";
         }
-        main.getAppointments().add(new Appointment(costumerName, year, month, day, hour, minute, Double.toString(totalPrice), addedProduct));
+        main.getAppointments().add(new Appointment(customerName, stringUserYear, stringUserMonth, stringUserDay, stringUserHour, stringUserMinute, Double.toString(totalPrice), addedProduct));
         UI.println("Appointment added successfully! :D");
 
         fileHandlingAppointment.saveAppointmentsToFile();
@@ -75,22 +73,18 @@ public class ModifyAppointment {
     // Delete appointments. creates a new arraylist (updatedAppointments), if the size of updated list is smaller than original, an appointment was deleted.
     public void deleteAppointment() {
         UI.promptString(); // Scanner bug
-        UI.println("Enter customer name: ");
-        String customerName = UI.promptString();
-        UI.println("Enter appointment year (yyyy)");
-        String year = Integer.toString(UI.promptInt());
-        UI.println("Enter appointment month (m)");
-        String month = Integer.toString(UI.promptInt());
-        UI.println("Enter appointment day (d)");
-        String day = Integer.toString(UI.promptInt());
+        customerName = promptDate.promptCustomerName();
+        stringUserYear = promptDate.promptYear();
+        stringUserMonth = promptDate.promptMonth();
+        stringUserDay = promptDate.promptDay();
 
         updatedAppointments = new ArrayList<>();
 
         for (Appointment appointment : main.getAppointments()) {
             if (!(appointment.getCustomerName().equals(customerName)
-                    && appointment.getYear().equals(year)
-                    && appointment.getMonth().equals(month)
-                    && appointment.getDay().equals(day))) {
+                    && appointment.getYear().equals(stringUserYear)
+                    && appointment.getMonth().equals(stringUserMonth)
+                    && appointment.getDay().equals(stringUserDay))) {
                 // Keep appointments that don't match the one to be deleted
                 updatedAppointments.add(appointment);
             }
@@ -105,25 +99,20 @@ public class ModifyAppointment {
         }
     }
 
-
     // View appointments on a specific date
     public void viewAppointment() {
         sortDate.sortAllAppointments();
+
         UI.promptString(); // Scanner bug
-        UI.println("Enter year (yyyy)");
-        String userYear = Integer.toString(UI.promptInt());
-
-        UI.println("Enter month (m)");
-        String userMonth = Integer.toString(UI.promptInt());
-
-        UI.println("Enter day (d)");
-        String userDay = Integer.toString(UI.promptInt());
+        stringUserYear = promptDate.promptYear();
+        stringUserMonth = promptDate.promptMonth();
+        stringUserDay = promptDate.promptDay();
 
 
         for (Appointment appointment : main.getAppointments()) // Iterates through each appointment in appointments, and checks if it contains user input.
-            if (appointment.getYear().contentEquals(userYear)
-                    && appointment.getMonth().contentEquals(userMonth)
-                    && appointment.getDay().contentEquals(userDay)) {
+            if (appointment.getYear().contentEquals(stringUserYear)
+                    && appointment.getMonth().contentEquals(stringUserMonth)
+                    && appointment.getDay().contentEquals(stringUserDay)) {
                 systemMessages.printAppointment(appointment);
             }
         UI.println(""); // Empty line
@@ -139,67 +128,58 @@ public class ModifyAppointment {
     }
 
     public void editAppointment() {
-        UI.promptString();
-        UI.println("Enter Costumer name");
-        String costumerName = UI.promptString();
-        UI.println("Enter Years");
-        String currentYear = UI.promptString();
-        UI.println("Enter month");
-        String currentMonth = UI.promptString();
-        UI.println("Enter day");
-        String currentDay = UI.promptString();
-        UI.println("Enter hours");
-        String currentHour = UI.promptString();
-        UI.println("Enter Minute");
-        String currenMinute = UI.promptString();
-        UI.println("Enter added product");
-        String currentProduct = UI.promptString();
+        UI.promptString(); // Scanner bug
+        customerName = promptDate.promptCustomerName();
+        currentYear = promptDate.promptYear();
+        currentMonth = promptDate.promptMonth();
+        currentDay = promptDate.promptDay();
+        currentHour = promptDate.promptHour();
+        currentMinute = promptDate.promptMinute();
+        UI.promptString(); // Scanner bug
+        currentProduct = promptDate.promptProduct();
 
         Appointment appointmentToEdit = null;
         for (Appointment appointment : main.getAppointments()) {
-            if (appointment.getCustomerName().equals(costumerName) &&
+            if (appointment.getCustomerName().equals(customerName) &&
                     (appointment.getYear().equals(currentYear) &&
                             (appointment.getMonth().equals(currentMonth) &&
                             (appointment.getDay().equals(currentDay) &&
                                     (appointment.getHour().equals(currentHour) &&
-                                     (appointment.getMinute().equals(currenMinute) &&
+                                     (appointment.getMinute().equals(currentMinute) &&
                                      (appointment.getAddedProduct().equals(currentProduct)))))))) {
                 appointmentToEdit = appointment;
 
-                if (appointmentToEdit == null) {
-                    UI.println("No appointment found");
+                UI.print(ConsoleColors.YELLOW_BOLD + "\nENTER NEW INFO");
+                UI.println(ConsoleColors.YELLOW);
+                newName = promptDate.promptCustomerName();
+                newYear = promptDate.promptYear();
+                UI.print(ConsoleColors.YELLOW);
+                newMonth = promptDate.promptMonth();
+                UI.print(ConsoleColors.YELLOW);
+                newDay = promptDate.promptDay();
+                UI.print(ConsoleColors.YELLOW);
+                newHour = promptDate.promptHour();
+                UI.print(ConsoleColors.YELLOW);
+                newMinute = promptDate.promptMinute();
+                UI.promptString(); // Scanner bug
+                newProduct = promptDate.promptProduct();
+                UI.print(ConsoleColors.RESET);
 
-                } else {
-                    UI.println("Enter new name");
-                    String newName = Integer.toString(UI.promptInt());
-                    UI.println("Enter new year");
-                    String newYear = Integer.toString(UI.promptInt());
-                    UI.println("enter new month");
-                    String newMonth = Integer.toString(UI.promptInt());
-                    UI.println("enter new day");
-                    String newDay = Integer.toString(UI.promptInt());
+                UI.println(ConsoleColors.GREEN_BRIGHT + "Successfully edited appointment!\n" + ConsoleColors.RESET);
 
-                    UI.println("Enter new hour");
-                    String newHour = Integer.toString(UI.promptInt());
-                    UI.println("Enter new minute");
-                    String newMinute = Integer.toString(UI.promptInt());
-                    UI.println("Enter New added products");
-                    String newProduct = Integer.toString(UI.promptInt());
+                appointmentToEdit.setName(newName);
+                appointmentToEdit.setYear(newYear);
+                appointmentToEdit.setMonth(newMonth);
+                appointmentToEdit.setDay(newDay);
+                appointmentToEdit.setHour(newHour);
+                appointmentToEdit.setMinute(newMinute);
+                appointmentToEdit.setAddedProduct(newProduct);
 
-                    appointmentToEdit.setName(newName);
-                    appointmentToEdit.setYear(newYear);
-                    appointmentToEdit.setMonth(newMonth);
-                    appointmentToEdit.setDay(newDay);
-                    appointmentToEdit.setHour(newHour);
-                    appointmentToEdit.setMinute(newMinute);
-                    appointmentToEdit.setAddedProduct(newProduct);
-
-                    fileHandlingAppointment.saveAppointmentsToFile();
-
-
-
+                fileHandlingAppointment.saveAppointmentsToFile();
                 }
             }
+        if (appointmentToEdit == null) {
+            UI.println(ConsoleColors.RED + "No appointment found\n" + ConsoleColors.RESET);
         }
     }
 }
