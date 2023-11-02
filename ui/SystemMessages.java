6a0f9d3b27e78eb689c65e98704fdfdb3a04f2c8
David Lu <davidlu1999@gmail.com>
2023-11-02 10:44:48 +0100

    Added printDailyTurnover(), updated UI

--- ui/SystemMessages.java
+++ ui/SystemMessages.java
@@ -13,9 +13,19 @@ public class SystemMessages {
         this.main = main;
     }
 
+    // Print opening hours
+    public void printOpeningHours() {
+        UI.println(ConsoleColors.BLUE_BRIGHT + "Opening hours: 10-18" + ConsoleColors.RESET);
+    }
+
+    // Print user date dates
+    public void printUserDate(int year, int month, int day, String dayOfWeek) {
+        UI.printf("%s%04d-%02d/%02d, %s: %s", ConsoleColors.YELLOW, year, month, day, dayOfWeek, ConsoleColors.RESET);
+    }
+
     // Prints appointment information
     public void printAppointment(Appointment appointment) {
-        UI.printf("%s%04d-%02d/%02d%s, %02d:%02d: %s, %s%n", ConsoleColors.YELLOW,
+        UI.printf("%s%04d-%02d/%02d%s, %02d:%02d: %-10s (Added product:   %s%7s%s)%n", ConsoleColors.YELLOW,
                 Integer.parseInt(appointment.getYear()),
                 Integer.parseInt(appointment.getMonth()),
                 Integer.parseInt(appointment.getDay()),
@@ -23,18 +33,39 @@ public class SystemMessages {
                 Integer.parseInt(appointment.getHour()),
                 Integer.parseInt(appointment.getMinute()),
                 appointment.getCustomerName(),
-                appointment.getAddedProduct());
+                ConsoleColors.BLUE_BRIGHT,
+                appointment.getAddedProduct(),
+                ConsoleColors.RESET);
     }
 
-    // Prints available date information
-    public void printAvailableDate(Appointment availableDate) {
-        UI.printf("%s%04d-%02d/%02d%s, %02d:%02d%n", ConsoleColors.YELLOW,
-                Integer.parseInt(availableDate.getYear()),
-                Integer.parseInt(availableDate.getMonth()),
-                Integer.parseInt(availableDate.getDay()),
+    // Prints appointment with financial information
+    public void printFinancialAppointment(Appointment appointment) {
+        UI.printf("%s%04d-%02d/%02d%s, %02d:%02d: %-10s Total price: %s%.2f DKK%s, (Added product:   %s%7s%s)%n", ConsoleColors.YELLOW,
+                Integer.parseInt(appointment.getYear()),
+                Integer.parseInt(appointment.getMonth()),
+                Integer.parseInt(appointment.getDay()),
                 ConsoleColors.RESET,
-                Integer.parseInt(availableDate.getHour()),
-                Integer.parseInt(availableDate.getMinute()));
+                Integer.parseInt(appointment.getHour()),
+                Integer.parseInt(appointment.getMinute()),
+                appointment.getCustomerName() + ",",
+                ConsoleColors.GREEN_BRIGHT,
+                appointment.getTotalPrice(),
+                ConsoleColors.RESET,
+                ConsoleColors.BLUE_BRIGHT,
+                appointment.getAddedProduct(),
+                ConsoleColors.RESET);
+    }
+
+    // Prints daily turnover information
+    public void printDailyTurnover(String year, String month, String day, double totalTurnover) {
+        UI.printf("The total turnover for %s%04d-%02d/%02d%s is: %s%.2f DKK%s%n%n", ConsoleColors.YELLOW,
+                Integer.parseInt(year),
+                Integer.parseInt(month),
+                Integer.parseInt(day),
+                ConsoleColors.RESET,
+                ConsoleColors.GREEN_BRIGHT,
+                totalTurnover,
+                ConsoleColors.RESET);
     }
 
 
@@ -50,6 +81,16 @@ public class SystemMessages {
     public void tryAgain() {
         UI.println(ConsoleColors.RED + "Try again!" + ConsoleColors.RESET);
         UI.promptString(); // User prompt - Enter to show menu again
+    }
+
+    // Print green colored text
+    public void printGreenColoredText(String text) {
+        UI.println(ConsoleColors.GREEN_BRIGHT + text + ConsoleColors.RESET);
+    }
+
+    // Print red colored text
+    public void printRedColoredText(String text) {
+        UI.println(ConsoleColors.RED + text + ConsoleColors.RESET);
     }
 
     // Exit system
