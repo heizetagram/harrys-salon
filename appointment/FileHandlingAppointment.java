f2141043a7a757fb21a9c058bd216ed9cdcb5cca
David Lu <davidlu1999@gmail.com>
2023-11-02 19:55:17 +0100

    Reverted bufferedReader to Scanner

--- appointment/FileHandlingAppointment.java
+++ appointment/FileHandlingAppointment.java
@@ -3,6 +3,7 @@ package appointment;
 import harryssalon.Main;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class FileHandlingAppointment {
     private Main main;
@@ -15,16 +16,18 @@ public class FileHandlingAppointment {
 
     // Read appointments logic
     public void loadAppointmentsFromFile() {
-        try (BufferedReader reader = new BufferedReader(new FileReader("appointments.txt"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                parts = line.split(",");
-                if (parts.length == 8) {
-                    main.getAppointments().add(new Appointment(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7]));
+        File file = new File("appointments.txt"); {
+            try (Scanner scanner = new Scanner(file)) {
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    parts = line.split(",");
+                    if (parts.length == 8) {
+                        main.getAppointments().add(new Appointment(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7]));
+                    }
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
