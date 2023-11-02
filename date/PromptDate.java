b0fc702d74f66c9035178ae0363955be3277972c
David Lu <davidlu1999@gmail.com>
2023-11-02 19:21:29 +0100

    Updated comments

--- date/PromptDate.java
+++ date/PromptDate.java
@@ -15,7 +15,7 @@ public class PromptDate {
     // Entered year must be between 2023-2024
     public String promptYear() {
         int intUserYear = 0;
-        UI.print("Enter year (yyyy): ");
+        UI.print("Enter year (2023-2024): ");
 
         while (intUserYear < 2023 || intUserYear > 2024) {
             intUserYear = UI.promptInt();
@@ -31,7 +31,7 @@ public class PromptDate {
     // Entered month must be between 1-12
     public String promptMonth() {
         int intUserMonth = 0;
-        UI.print("Enter month (m): ");
+        UI.print("Enter month (1-12): ");
 
         while (intUserMonth < 1 || intUserMonth > 12) {
             intUserMonth = UI.promptInt();
@@ -47,7 +47,7 @@ public class PromptDate {
     // Entered day must be between 1-31
     public String promptDay() {
         int intUserDay = 0;
-        UI.print("Enter day (d): ");
+        UI.print("Enter day (1-31): ");
 
         while (intUserDay < 1 || intUserDay > 31) {
             intUserDay = UI.promptInt();
@@ -63,7 +63,7 @@ public class PromptDate {
     // Entered hour must be between 10-17
     public String promptHour() {
         int intUserHour = 0;
-        UI.print("Enter hour (h): ");
+        UI.print("Enter hour (10-17): ");
 
         while (intUserHour < 10 || intUserHour > 17) {
             intUserHour = UI.promptInt();
@@ -79,7 +79,7 @@ public class PromptDate {
     // Entered minute must either 00 or 30
     public String promptMinute() {
         int intUserMinute = 1;
-        UI.print("Enter minute (m): ");
+        UI.print("Enter minute (00 or 30): ");
 
         while (intUserMinute != 0 && intUserMinute != 30) {
             intUserMinute = UI.promptInt();
